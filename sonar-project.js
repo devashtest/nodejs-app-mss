36751d6746a310fc3b2a4ec4b58079ddaf86224b
devashtest <reddy97.ashwath@gmail.com>
2024-11-17 10:07:55 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_ecc45645d0c4c1356773c7645c210aa22d67e064',
+	    'sonar.login': 'squ_ade83a7a9e42e06272df508696353a26051ebaa2',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
